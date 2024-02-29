e146dc352ebd1cd1bc3f82cf0d6ae9358760340d
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2024-02-28 22:41:20 -0300

    getMessage function

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const cookie = document.querySelector("#cookie");
 const screen1 = document.querySelector(".screen1");
 const screen2 = document.querySelector(".screen2");
 const reset = document.querySelector("#reset");
+const message = document.querySelector("#message");
 const cookieMessages = [
   "Se alguém está tão cansado que não possa te dar um sorriso, deixa-lhe o teu.",
   "Procure acender uma vela em vez de amaldiçoar a escuridão",
@@ -14,6 +15,7 @@ reset.addEventListener("click", resetCookie)
 
 function showMessage(event) {
   event.preventDefault();
+  getMessage()
   screen1.classList.toggle("hide")
   screen2.classList.toggle("hide")
 }
@@ -22,4 +24,10 @@ function resetCookie(event) {
   event.preventDefault();
   screen1.classList.toggle("hide")
   screen2.classList.toggle("hide")
+}
+
+function getMessage() {
+  let messageIndex = Math.floor(Math.random() * cookieMessages.length)
+  message.innerHTML = cookieMessages[messageIndex]
+  console.log(messageIndex)
 }
