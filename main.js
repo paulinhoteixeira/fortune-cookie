0aeb1227c68060c0a9abf293886eb4cb054556b1
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2024-02-28 22:30:31 -0300

    animation for the cookies message

--- main.js
+++ main.js
@@ -2,12 +2,17 @@ const cookie = document.querySelector("#cookie");
 const screen1 = document.querySelector(".screen1");
 const screen2 = document.querySelector(".screen2");
 const reset = document.querySelector("#reset");
+const cookieMessages = [
+  "Se alguém está tão cansado que não possa te dar um sorriso, deixa-lhe o teu.",
+  "Procure acender uma vela em vez de amaldiçoar a escuridão",
+  "Life long learning"
+]
 
 
-cookie.addEventListener("click", showTicket)
+cookie.addEventListener("click", showMessage)
 reset.addEventListener("click", resetCookie)
 
-function showTicket(event) {
+function showMessage(event) {
   event.preventDefault();
   screen1.classList.toggle("hide")
   screen2.classList.toggle("hide")
